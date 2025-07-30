a2e869671c903080b03fac60c51c9f95e9048bd7
josniel <josnielramos@gmail.com>
2025-07-30 19:29:30 -0400

    task4 first commit

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+4-define_variables.py
+
+Define and annotate four variables:
+    - a: integer
+    - pi: float
+    - i_undestand_annotations: boolean
+    - school: string
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
