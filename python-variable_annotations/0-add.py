e53300ec2bb733fd4baaa65723db9d312de16a98
josniel <josnielramos@gmail.com>
2025-07-30 18:44:53 -0400

    task0 first commit

---
+++ python-variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-add.py: Define a function that adds two floats,
+with full type annotations.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Return the sum of a and b.
+
+    Args:
+        a (float): The first addend.
+        b (float): The sencod addend.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
