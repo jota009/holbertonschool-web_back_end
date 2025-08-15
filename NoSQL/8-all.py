600e1fb249a70c92e12c1c631858ba2b175532e8
josniel <josnielramos@gmail.com>
2025-08-15 14:38:16 -0400

    task 8-testfix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-"""Lists all documents in a collection."""
-
-from typing import List, Dict
-from pymongo.collection import Collection
+"""Lists all documents in a MongoDB collection."""
 
 
-def list_all(mongo_collection: Collection) -> List[Dict]:
-    """Return all documents in the collection as a list.
-    (or [] if none).
-
-    Args:
-        mongo_collection: A PyMongo collection object.
-
-    Returns:
-        A list of documents (dicts). Empty list if none exist.
-    """
+def list_all(mongo_collection):
+    """Return all documents as a list (empty list if none)."""
     return list(mongo_collection.find())
