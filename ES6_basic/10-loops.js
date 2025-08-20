dcd1ad75c752b6a54964ef201ed186b395702853
josniel <josnielramos@gmail.com>
2025-08-20 15:42:30 -0400

    task10-testoutpu tfix

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [index, value] of array.entries()) {
-    array[index] = appendString + value;
-  }
-
-  return array;
+  return array.map(value => appendString + value);
 }
