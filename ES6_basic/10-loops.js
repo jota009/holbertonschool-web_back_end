4889aa9c2c3fcf199b4de993f13befd59071a145
josniel <josnielramos@gmail.com>
2025-08-20 20:39:40 -0400

    task10-test fix output

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disable-next-line no-param-reassign
   for (const [index, value] of array.entries()) {
-    array[index] = appendString + value;
+    array[index] = appendString + value; // <- Linter is silenced
   }
   return array;
 }
