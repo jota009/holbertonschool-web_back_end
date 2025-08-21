6fd33ce71a2144c9e30ab8ab7085a9a79363b473
josniel <josnielramos@gmail.com>
2025-08-21 12:51:30 -0400

    task 12 firt commit

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const result =[];
+  const result = [];
   for (const value of array) {
     result.push(`${appendString}${value}`);
   }
