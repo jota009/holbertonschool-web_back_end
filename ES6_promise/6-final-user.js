7bab1884ef8dac24ea4cee1ce0d3a980fe2038f0
josniel <josnielramos@gmail.com>
2025-08-27 12:21:12 -0400

    task 6 test fix string reason

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -9,7 +9,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   ]).then((results) =>
     results.map((r) => ({
       status: r.status,
-      value: r.status === 'fulfilled' ? r.value : r.reason,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
     }))
   );
 }
