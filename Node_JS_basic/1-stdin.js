2f7026336210a31cbe40ec9a5d2183bedd7fd1dc
josniel <josnielramos@gmail.com>
2025-09-02 19:22:28 -0400

    task 1-frst test commit

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (chunk) => {
+  process.stdout.write(`Your name is: ${chunk}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
+
